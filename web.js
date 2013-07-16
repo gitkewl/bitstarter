fcccac4963270ee5651f23f9981f7f4274883435
gitkewl <geddie81@gmail.com>
2013-07-16 12:05:22 +0000

    Updated file to use Web Browser

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
-var infile = "index.html";
-var content = fs.readFileSync(infile);
-var strContent = content.toString();
 
 app.get('/', function(request, response) {
-  response.send(strContent);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
