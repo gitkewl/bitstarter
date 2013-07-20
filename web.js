a62b36691a2b28e0442c7710aa3be124100280e3
gitkewl <geddie81@gmail.com>
2013-07-20 18:48:35 -0400

    Updates

--- web.js
+++ web.js
@@ -5,11 +5,18 @@ var htmlfile = "index.html";
 var app = express();
 
 app.get('/', function(request, response) {
-    var html = fs.readFileSync(htmlfile).toString();
+    var html = fs.readFileSync(htmlfile).toString('utf-8');
     response.send(html);
 });
 
 var port = process.env.PORT || 8080;
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static/css'));
+app.use(express.static(__dirname + '/static/fonts'));
+app.use(express.static(__dirname + '/static/images'));
+app.use(express.static(__dirname + '/static/javascript'));
+app.use(express.static(__dirname + '/static/media'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
