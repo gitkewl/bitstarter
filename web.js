32a22bbc0d3ec317db7a9fa8d279e298846ec69c
gitkewl <geddie81@gmail.com>
2013-07-20 23:27:58 +0000

    Updated file

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
 
-var app = express();
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString('utf-8');
